6dc0d3c3d34375c63d019d05bad48cfa7640a984
Kyle Iacomacci <iacomaccikyle4@gmail.com>
2026-02-06 15:05:40 -0500

    Add files via upload

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -3,16 +3,16 @@ import pandas as pd
 from sklearn.ensemble import GradientBoostingClassifier
 
 # set seed
-seed = 316
+seed = 314
 
 def train_model(X, y, seed=seed):
     """
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.1,
-        n_estimators=100000,
-        max_depth=None,
+        learning_rate=0.5,
+        n_estimators=1000,
+        max_depth=2,
         subsample=1,
         min_samples_leaf=1,
         random_state=seed
