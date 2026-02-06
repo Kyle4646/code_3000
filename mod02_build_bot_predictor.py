5a71484e06385526489a2d76d842ebeb4f585e28
Kyle Iacomacci <iacomaccikyle4@gmail.com>
2026-02-06 15:09:50 -0500

    Add files via upload

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,8 +10,8 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.5,
-        n_estimators=1000,
+        learning_rate=0.3,
+        n_estimators=10000,
         max_depth=2,
         subsample=1,
         min_samples_leaf=1,
